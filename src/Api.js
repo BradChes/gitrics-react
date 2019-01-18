68f15473c432f4edb1be24105588e69f50a95b25
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-01-18 19:22:03 +0000

    Added Fix branch
    Fix branch now shows up in ui.

--- src/Api.js
+++ src/Api.js
@@ -2,30 +2,53 @@ import React, { Component } from 'react';
 
 class App extends Component {
     state = {
-        data: [],
-        size: null
+        all: [],
+        allSize: null,
+        fix: [],
+        fixSize: null
+
     };
 
     // Code is invoked after the component is mounted/inserted into the DOM tree.
     componentDidMount() {
-        const url = "/branches";
-        fetch(url)
+        const allUrl = "/branches";
+        fetch(allUrl)
             .then(result => result.json())
             .then(result => {
                 this.setState({
-                  data: result.branches,
-                  size: result.size
+                  all: result.branches,
+                  allSize: result.size
                 })
             });
+
+            const fixUrl = "/branches/fix";
+            fetch(fixUrl)
+                .then(result => result.json())
+                .then(result => {
+                    this.setState({
+                      fix: result.branches,
+                      fixSize: result.size
+                    })
+                });
     }
 
     render() {
-        const data = this.state.data;
-        const result = data.map((entry, index) => {
-            return <li key={index}>{entry.name}, {this.state.size}</li>;
+        const all = this.state.all;
+        const allResult = all.map((entry, index) => {
+            return <li key={index}>{entry.name}, {this.state.allSize}</li>;
         });
 
-        return <ul>{result}</ul>;
+        const fix = this.state.fix;
+        const fixResult = fix.map((entry, index) => {
+            return <li key={index}>{entry.name}, {this.state.fixSize}</li>;
+        });
+
+        return (
+          <div className="container">
+            <ul>{allResult}</ul>
+            <ul>{fixResult}</ul>
+          </div>
+        )
     }
 }
 
