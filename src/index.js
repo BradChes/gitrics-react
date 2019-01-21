76f972fed8e99c5a3c2baa0a5135369b1769fc63
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-01-21 09:42:24 +0000

    Removed unecessary files for css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Api';
-import './index.css';
 import './bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
