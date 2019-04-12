b16a52d23e21ae3cd2df11dcaa526ff62d960d2c
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2019-04-12 14:57:47 +0100

    Added a loading spinner
    added a loading screen. doesnt work as of yet.
    
    Added promise for mulitple fetches.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-          <h1> Welcome to Gitrics </h1>
+          <h1>Welcome to Gitrics</h1>
       </div>
     )
   }
--- src/RepoCards.js
+++ src/RepoCards.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import BranchStatus from './BranchStatus';
+import LoadingSpinner from './LoadingSpinner';
 import './style.css';
 
 class RepoCards extends Component {
-  state = {
-    unmerged: [],
-    merged: [],
-    stale: [],
-    allLifetime: String,
-    featureLifetime: String,
-    spikeLifetime: String,
-    fixLifetime: String
-  };
+
+  constructor() {
+    super()
+    this.state = {
+      unmerged: 0,
+      merged: 0,
+      stale: 0,
+      allLifetime: 0,
+      featureLifetime: 0,
+      spikeLifetime: 0,
+      fixLifetime: 0,
+      loading: false
+    };
+  }
   
   componentDidMount() {
     const { match: { params } } = this.props;
@@ -20,41 +26,26 @@ class RepoCards extends Component {
     const staleUrl = `${params.repoName}/branches/stale`;
     const lifetimeUrl = `${params.repoName}/branches/lifetime`;
 
-    fetch(unmergedUrl)
-      .then(result => result.json())
-      .then(result => {
-        console.log(params)
-        this.setState({
-        unmerged: result.branches
-        })
-      });
-  
-    fetch(mergedUrl)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-        merged: result.branches
-        })
-      });
-  
-    fetch(staleUrl)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-        stale: result.branches
-        })
-      });  
-
-    fetch(lifetimeUrl)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-          allLifetime: result.allLifetime,
-          featureLifetime: result.featureLifetime,
-          spikeLifetime: result.spikeLifetime,
-          fixLifetime: result.fixLifetime
-        })
-      });
+    this.setState({loading: true});
+    Promise.all([
+      fetch(unmergedUrl),
+      fetch(mergedUrl),
+      fetch(staleUrl),
+      fetch(lifetimeUrl)
+    ])
+    .then(([res1, res2, res3, res4]) => ([res1.json(), res2.json(),res3.json(), res4.json()]))
+    .then(([res1, res2, res3, res4]) => {
+      console.log(res1)
+      this.setState({
+      unmerged: res1.size,
+      merged: res2.size,
+      stale: res3.size,
+      allLifetime: res4.allLifetime,
+      featureLifetime: res4.featureLifetime,
+      spikeLifetime: res4.spikeLifetime,
+      fixLifetime: res4.fixLifetime,
+      loading: false
+    })});
   }
   
   render() {
@@ -66,26 +57,31 @@ class RepoCards extends Component {
     const featureLifetime = this.state.featureLifetime;
     const spikeLifetime = this.state.spikeLifetime;
     const fixLifetime = this.state.fixLifetime;
+    const loading = this.state.loading
   
     return (
       <div className="container">
         <h1>Gitrics for: {params.repoName}</h1>
-        <h3>Branch Type Sizes:</h3>
-        <div className="cardArea">
-          <BranchStatus branchSize={merged.length} branchName={"Merged"}/>
-          <BranchStatus branchSize={stale.length} branchName={"Stale"}/>
-          <BranchStatus branchSize={unmerged.length} branchName={"Unmerged"}/>
-        </div>
-        <h3>Average Branch Lifetime:</h3>
-        <div className="cardArea">
-          <BranchStatus branchSize={allLifetime} branchName={"All"}/>
-          <BranchStatus branchSize={featureLifetime} branchName={"Features"}/>
-          <BranchStatus branchSize={spikeLifetime} branchName={"Spike"}/>
-          <BranchStatus branchSize={fixLifetime} branchName={"Fix"}/>
-        </div>
+        {loading ? <LoadingSpinner/> : 
+          <div>
+              <h3>Branch Type Sizes:</h3>
+              <div className="cardArea">
+                <BranchStatus branchSize={merged} branchName={"Merged"}/>
+                <BranchStatus branchSize={stale} branchName={"Stale"}/>
+                <BranchStatus branchSize={unmerged} branchName={"Unmerged"}/>
+              </div>
+              <h3>Average Branch Lifetime:</h3>
+              <div className="cardArea">
+                <BranchStatus branchSize={allLifetime} branchName={"All"}/>
+                <BranchStatus branchSize={featureLifetime} branchName={"Features"}/>
+                <BranchStatus branchSize={spikeLifetime} branchName={"Spike"}/>
+                <BranchStatus branchSize={fixLifetime} branchName={"Fix"}/>
+              </div>
+            </div>
+        }
       </div>
     )
   }
 }
 
-  export default RepoCards;
+export default RepoCards;
